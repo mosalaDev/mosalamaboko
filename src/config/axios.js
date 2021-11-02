bdc9a4786f08905d989655b169cce1a58fee0efc
samkindev <samkyunyudev@gmail.com>
2021-11-02 10:58:29 -0900

    Some change

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,14 +2,20 @@ import axios from 'axios';
 
 const token = localStorage.getItem('user');
 
-const a = axios.create({
-    baseURL: 'https://mosala.herokuapp.com/api',
-});
-
 // const a = axios.create({
-//     baseURL: 'http://localhost:5500/api',
+//     baseURL: 'https://mosala.herokuapp.com/api',
 // });
 
-a.defaults.headers.common['Authorization'] = typeof token === "string" ? token.length > 0 ? "Bearer " + token : null : null;
+console.log(token);
+
+const a = axios.create({
+    baseURL: 'http://localhost:5500/api',
+});
+
+a.interceptors.request.use(function (config) {
+    config.headers.Authorization = (typeof token === "string" ? token.length > 0 ? "Bearer " + token : null : null);
+
+    return config;
+});
 
 export default a;
