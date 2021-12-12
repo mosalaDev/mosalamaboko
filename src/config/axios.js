798edc2ad8c93296441ca51505650b6d9e5dccb7
samkindev <samkyunyudev@gmail.com>
2021-12-12 17:13:50 +0100

    Some modifications

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-// const a = axios.create({
-//         baseURL: 'https://mosala.herokuapp.com/api',
-//     });
-    
 const a = axios.create({
-    baseURL: 'http://localhost:5500/api',
-});
+        baseURL: 'https://mosala.herokuapp.com/api',
+    });
+    
+// const a = axios.create({
+//     baseURL: 'http://localhost:5500/api',
+// });
     
     
 a.interceptors.request.use(function (config) {
