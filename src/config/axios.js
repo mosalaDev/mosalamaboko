64b9da311551e430e46a4b3a895da57d8150f17c
mosalaDev <92552880+mosalaDev@users.noreply.github.com>
2023-03-17 21:31:02 +0100

    Change the api url

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const a = axios.create({
-    baseURL: 'https://mosalamabokoapi.up.railway.app/api',
+    baseURL: 'https://mosalaapi.up.railway.app/api',
 });
 
 // const a = axios.create({
