b4c2c78a1ed1859d8d1fa30d6a43010dd44f0874
Sam <samkyunyudev@gmail.com>
2023-06-06 11:49:42 +0100

    Base url changed

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const a = axios.create({
-    baseURL: 'https://mosalaapi.up.railway.app/api',
+    baseURL: 'https://mosalamabokoapi.up.railway.app/api',
 });
 
 // const a = axios.create({
